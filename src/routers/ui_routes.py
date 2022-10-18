f9159b469aded01ccf6874557a364e0271a6c3d8
barreraalexander <barreraalexander93@gmail.com>
2022-10-18 14:25:53 -0400

    adding update to base

--- src/routers/ui_routes.py
+++ src/routers/ui_routes.py
@@ -19,6 +19,20 @@ def index(request: Request, response_model=HTMLResponse):
 
 
 @router.get("/products", status_code=status.HTTP_200_OK)
-def products_index(request: Request, response_model=HTMLResponse):
-    return TEMPLATES.TemplateResponse("ecommerce/index.html", {"request" : request})
+def index_products(request: Request, response_model=HTMLResponse):
+    # featured_product = []
+    featured_product = {
+        "name" : "TestName",
+        "price" : 10.07,
+        "currency" : "US Dollars",
+        "info" : "This test product is clean",
+        "short_description" : "made in the mountains of Hispaniola",
+        "long_description" : "made in the mountains of Hispaniola, where the river runs wild and wide and full of color",
+    }
+
+    return TEMPLATES.TemplateResponse("ecommerce/index.html", {
+        "request" : request,
+        "featured_product" : featured_product
+        })
+
 
