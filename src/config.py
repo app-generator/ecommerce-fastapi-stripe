7760c84ddde3eaa32151130b22b4e8b3aa727fc8
barreraalexander <barreraalexander93@gmail.com>
2022-10-18 13:23:49 -0400

    adding base of the project

---
+++ src/config.py
@@ -0,0 +1,23 @@
+# # -*- encoding: utf-8 -*-
+# """
+# Copyright (c) 2019 - present AppSeed.us
+# """
+
+# import os
+
+# class Config(object):
+
+#     basedir = os.path.abspath(os.path.dirname(__file__))
+
+#     DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+
+#     # App Config - the minimal footprint
+#     SECRET_KEY = os.getenv('SECRET_KEY', 'S#perS3crEt_9999')
+
+#     STRIPE_SECRET_KEY      = os.getenv('STRIPE_SECRET_KEY'     , None )
+#     STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY', None )
+#     SERVER_ADDRESS         = os.getenv('SERVER_ADDRESS', 'http://localhost:5000/')
+
+#     STRIPE_IS_ACTIVE       = False
+#     if STRIPE_SECRET_KEY and STRIPE_PUBLISHABLE_KEY:
+#         STRIPE_IS_ACTIVE = True
---
+++ src/static/assets/js/payments.js
@@ -0,0 +1,29 @@
+
+// Get Stripe publishable key
+fetch("/config")
+.then((result) => { return result.json(); })
+.then((data) => {
+  // Initialize Stripe.js
+  const stripe = Stripe(data.publicKey);
+
+  // Event handler
+  document.querySelector("#submitBtn").addEventListener("click", () => {
+
+    var product = document.getElementById('product_slug').innerText;
+
+    //console.log( ' >>> Product = ' + product );
+    //return false;
+
+    // Get Checkout Session ID
+    fetch("/create-checkout-session/" + product)
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      console.log(data);
+      // Redirect to Stripe Checkout
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  });
+});
