55112274727ff4557f56838cd5ca9610eaa8d18b
barreraalexander <barreraalexander93@gmail.com>
2022-10-18 13:43:09 -0400

    added base app

--- src/app.py
+++ src/app.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from src.config import settings
+
+
+if (settings.debug=='True'):
+    pass
+
 app = FastAPI(debug=True, reload=True)
 
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
