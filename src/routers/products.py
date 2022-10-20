4d8686444de3774d5ff1ef45414515cf75a7f474
barreraalexander <barreraalexander93@gmail.com>
2022-10-19 22:02:53 -0400

    now able to get item by slug

--- src/routers/products.py
+++ src/routers/products.py
@@ -20,17 +20,34 @@ async def get_products():
     return products
 
 
-@router.get("/{id}", response_model=schemas.Product)
-async def get_product(id: int):
+@router.get("/{slug}", response_model=schemas.Product)
+async def get_product_by_slug(slug: str):
 
     products = get_local_products()
 
     if not products:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product was not found")
-    
-    try:
-        product = products[id]
-    except:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product was not found")
 
-    return product
+    for product in products:
+        if product['slug']==slug:
+            return product
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product was not found")
+
+
+# @router.get("/{id}", response_model=schemas.Product)
+# async def get_product(id: int):
+
+#     products = get_local_products()
+
+#     if not products:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product was not found")
+    
+#     try:
+#         product = products[id]
+#     except IndexError:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product was not found")
+
+#     return product
+
+
--- src/routers/ui_routes.py
+++ src/routers/ui_routes.py
@@ -20,19 +20,15 @@ async def index(request: Request, response_model=HTMLResponse):
 
 @router.get("/products", status_code=status.HTTP_200_OK)
 async def products_index(request: Request, response_model=HTMLResponse):
-    featured_product_id = 0
+    featured_product_slug = 'featured'
     base_url = request.base_url
-    products_url = app.product_router.url_path_for("get_product", id=featured_product_id)
+    products_url = app.product_router.url_path_for("get_product_by_slug", slug=featured_product_slug)
     request_url = base_url.__str__() + products_url.__str__()[1:]
 
     http3client = http3.AsyncClient()
     response = await http3client.get(request_url)
 
     featured_product = response.json()
-
-    # print (featured_product['slug'])
-
-
 
     return TEMPLATES.TemplateResponse("ecommerce/index.html", {
         "request" : request,
