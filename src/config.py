e2b5d46c8712d1425b1b5fe01821e1332ef653f3
barreraalexander <barreraalexander93@gmail.com>
2022-10-21 13:09:11 -0400

    update to settings

--- src/config.py
+++ src/config.py
@@ -3,12 +3,21 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     debug: bool = False
     secret_key: str = 'S#perS3crEt_9999'
+    server_address: str = 'http://localhost:8000/'
+    
     stripe_secret_key: str = None
     stripe_publishable_key: str = None
-    server_address: str = 'localhost:8000'
+    stripe_is_active: bool = False
+    
 
-    
-    
+    def __init__(self):
+        super().__init__()
+        self.check_stripe()
+
+    def check_stripe(self):
+        if self.stripe_secret_key and self.stripe_publishable_key:
+            self.stripe_is_active = True
+
     class Config:
         env_file = "./.env"
 
